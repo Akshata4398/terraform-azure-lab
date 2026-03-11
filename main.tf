6bdb8a83957e019396a9a20ef3a8018cfca93f20
Akshata Kalmesh Mathapati <49336046+Akshata4398@users.noreply.github.com>
2026-03-11 13:45:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
+# test this code if you can see the latest
 terraform {
   required_providers {
     azurerm = {
